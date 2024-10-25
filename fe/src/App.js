1db606fe75e6fc25b1d750afb4e23003c110f84e
ma9241412 <Syed Muhammad Ali>
2024-10-25 16:47:07 +0500

    changes

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello this is saad altaf :</h1>
+      <h1>hello this is saad altaf qureshi</h1>
     </div>
   );
 }
