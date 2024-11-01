d39297eb06fe07ae4cbd4fe4cfbbe2edf2aff9e9
ma9241412 <Syed Muhammad Ali>
2024-11-01 08:25:23 +0500

    changes

--- fe/src/Components/Slider.js
+++ fe/src/Components/Slider.js
@@ -12,13 +12,14 @@ const images = [image1, image2, image3, image4];
 
 const FullWidthCarousel = () => {
   return (
-    <div className="carousel-container">
-      <Carousel arrows autoplay>
+    <div style={{ width: '100%', maxWidth: '100vw', overflow: 'hidden', margin: 0, padding: 0 }}>
+      <Carousel autoplay>
         {images.map((image, index) => (
-          <div key={index} className="carousel-slide">
+          <div key={index}>
             <img 
               src={image} 
               alt={`Slide ${index + 1}`} 
+              style={{ width: '100%', height: 'auto', maxHeight: '500px', objectFit: 'cover', display: 'block' }} 
               className="carousel-image"
             />
           </div>
