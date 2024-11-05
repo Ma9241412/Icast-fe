0cb0e3d6fbfc170ad52e581b7f110c2b07fce450
Ma9241412 <ma9241412@gmail.com>
2024-11-05 15:54:41 +0500

    changes

--- fe/src/Layouts/IcastHeader.js
+++ fe/src/Layouts/IcastHeader.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../Assets/logow.png";
+import logo from "../Assets/logoicast.png";
 import "../Styles/IcaseHeaderStyles.css";
-import { Grid, Image } from "antd";
+import { Divider, Grid, Image, Space, Typography } from "antd";
+import suparco from '../Assets/suparco.png';
+import ist from '../Assets/ist-logo.png';
+
 const { useBreakpoint } = Grid;
 
 const IcastHeader = () => {
   const screens = useBreakpoint();
-  const logoWidth = screens.lg ? 380 : screens.md ? 250 : 250;
+  const logoWidth = screens.lg ? 250 : screens.md ? 250 : 150;
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -23,13 +26,41 @@ const IcastHeader = () => {
     <header className="header">
       <div className="header-left">
         <div className="logo">
-          <Image preview={false} src={logo} width={logoWidth} alt="ICAST Logo" className="logo-image" />
+          <Space size={200} align="end">
+            <Image
+              preview={false}
+              src={logo}
+              width={screens.xs ? 150 : logoWidth}
+              alt="ICAST Logo"
+              className="logo-image"
+            />
+            {screens.md && (
+              <Space align="start" direction="vertical" className="event-info">
+                <Typography.Title style={{ color: "white", fontSize: "15px" }}>
+                  INTERNATIONAL CONFERENCE ON APPLICATION OF SPACE SCIENCE AND TECHNOLOGY
+                </Typography.Title>
+                <Typography.Title style={{ color: "white", fontSize: "15px" }}>
+                  SPACE AND SUSTAINABLE DEVELOPMENT
+                </Typography.Title>
+                <Typography.Title style={{ color: "white", fontSize: "15px" }}>
+                  NOV 18-20 ,2025
+                </Typography.Title>
+              </Space>
+            )}
+            {screens.md && (
+              <Space size={20}>
+                <Image width={100} src={suparco} alt="Suparco Logo" />
+                <Image width={150} src={ist} alt="IST Logo" />
+              </Space>
+            )}
+          </Space>
         </div>
+        {/* Desktop Menu */}
         <nav className="desktop-menu">
           <Link to="/about" className="menu-link">ABOUT ICAST</Link>
           <Link to="/themes" className="menu-link">CONFERENCE THEMES</Link>
           <Link to="/content" className="menu-link">CALL FOR CONTENT</Link>
-          <Link to="/registration" className="menu-link">REGISTRATION </Link>
+          <Link to="/registration" className="menu-link">REGISTRATION</Link>
           <Link to="/sponsors" className="menu-link">EXPOSITION & SPONSORS</Link>
           <Link to="/program" className="menu-link">PROGRAMME</Link>
           <Link to="/download" className="menu-link">DOWNLOADS & MANUALS</Link>
@@ -49,7 +80,8 @@ const IcastHeader = () => {
             <Link to="/about" onClick={handleCloseMenu} className="menu-link">About ICAST</Link>
             <Link to="/themes" onClick={handleCloseMenu} className="menu-link">Conference Theme</Link>
             <Link to="/content" onClick={handleCloseMenu} className="menu-link">Call for Content</Link>
-            <Link to="/registration" onClick={handleCloseMenu} className="menu-link">Exposition & Sponsors</Link>
+            <Link to="/registration" onClick={handleCloseMenu} className="menu-link">Registration</Link>
+            <Link to="/sponsors" onClick={handleCloseMenu} className="menu-link">Exposition & Sponsors</Link>
             <Link to="/program" onClick={handleCloseMenu} className="menu-link">Program</Link>
           </nav>
         </div>
